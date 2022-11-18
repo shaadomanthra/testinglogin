273c6ea3d7fb98e4a57e015bf3e7784909a310df
shaadomanthra <shaadomanthra@yahoo.com>
2022-11-18 17:03:44 +0530

    some changes

--- upload.sh
+++ upload.sh
@@ -6,9 +6,9 @@ if [!$1]
 then
 	git add *;
 	git commit -m "$1";
-	git push origin dev0;
+	git push origin main;
 else
 	git add *;
 	git commit -m "some changes";
-	git push origin dev0;
+	git push origin main;
 fi
