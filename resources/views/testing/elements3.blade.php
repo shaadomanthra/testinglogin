521ecf4031a31d01c83634b461c4952a0d531982
shaadomanthra <shaadomanthra@yahoo.com>
2022-11-30 14:43:58 +0530

    some cahnge

--- resources/views/testing/elements3.blade.php
+++ resources/views/testing/elements3.blade.php
@@ -2,11 +2,10 @@
   <div class="container">
     <h1 class="mt-5">Testing Demo - IFrame</h1>
     <div class="bg-white p-3 my-2">
-    <h4>Iframes</h4>
     <div class="bg-light p-3 my-3">
-      <iframe src="/buttons" title="description" width="100%" height="250px"></iframe>
+      <iframe src="/buttons" title="description" width="100%" height="300px"></iframe>
     </div>
-    <button id="btn_four" class="btn btn-danger">Button Four</button>
+    <button id="btn_four" class="btn btn-info">Button Four</button>
     <div class="alert alert_four alert-info mt-5" role="alert" style="display: none;">
       <h4 class="alert-heading">Well done!</h4>
       <p id="alert_message" class=" alert_message mb-0 pb-0">You have clicked <b>Button Four</b></p>
