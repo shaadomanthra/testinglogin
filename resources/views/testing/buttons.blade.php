9d2a005c9fafed91f3deb1f676eea61c9b688913
shaadomanthra <shaadomanthra@yahoo.com>
2022-11-30 14:44:27 +0530

    some cahnge

--- resources/views/testing/buttons.blade.php
+++ resources/views/testing/buttons.blade.php
@@ -10,15 +10,15 @@
     <button id="btn_two" class="btn btn-success mr-3">Button Two</button>
     <button id="btn_three" class="btn btn-danger">Button Three</button>
 
-    <div class="alert alert_one alert-primary mt-5" role="alert" style="display: none;">
+    <div class="alert alert_one alert-primary mt-2" role="alert" style="display: none;">
       <h4 class="alert-heading">Well done!</h4>
       <p id="alert_message" class=" alert_message mb-0 pb-0">You have clicked <b>Button One</b></p>
     </div>
-    <div class="alert alert_two alert-success mt-5" role="alert" style="display: none;">
+    <div class="alert alert_two alert-success mt-2" role="alert" style="display: none;">
       <h4 class="alert-heading">Well done!</h4>
       <p id="alert_message" class=" alert_message mb-0 pb-0">You have clicked <b>Button Two</b></p>
     </div>
-    <div class="alert alert_three alert-danger mt-5" role="alert" style="display: none;">
+    <div class="alert alert_three alert-danger mt-2" role="alert" style="display: none;">
       <h4 class="alert-heading">Well done!</h4>
       <p id="alert_message" class=" alert_message mb-0 pb-0">You have clicked <b>Button Three</b></p>
     </div>
